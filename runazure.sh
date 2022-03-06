8d3cbd91407bed91aae7f92787a65c83cdf80804
slicommeo <46085992+slicommeo@users.noreply.github.com>
2022-03-06 22:09:24 +0700

    Update runazure.sh

--- runazure.sh
+++ runazure.sh
@@ -4,7 +4,7 @@ az group create --name myResourceGroup01 --location southeastasia
 
 hello="$(az vm show -g myResourceGroup01   -n myVM00 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup01 -n myVM00 --yes
 elif [ -z "$hello" ]; then 
@@ -20,7 +20,7 @@ az group create --name myResourceGroup02 --location australiaeast
 
 hello="$(az vm show -g myResourceGroup02   -n myVM01 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup02 -n myVM01 --yes
 elif [ -z "$hello" ]; then 
@@ -35,7 +35,7 @@ az group create --name myResourceGroup03 --location canadacentral
 
 hello="$(az vm show -g myResourceGroup03   -n myVM02 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup03 -n myVM02 --yes
 elif [ -z "$hello" ]; then 
@@ -51,7 +51,7 @@ az group create --name myResourceGroup04 --location northeurope
 
 hello="$(az vm show -g myResourceGroup04   -n myVM03 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup04 -n myVM03 --yes
 elif [ -z "$hello" ]; then 
@@ -66,7 +66,7 @@ az group create --name myResourceGroup05 --location westeurope
 
 hello="$(az vm show -g myResourceGroup05   -n myVM04 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup05 -n myVM04 --yes
 elif [ -z "$hello" ]; then 
@@ -82,7 +82,7 @@ az group create --name myResourceGroup06 --location centralindia
 
 hello="$(az vm show -g myResourceGroup06   -n myVM05 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup06 -n myVM05 --yes
 elif [ -z "$hello" ]; then 
@@ -98,7 +98,7 @@ az group create --name myResourceGroup07 --location japaneast
 
 hello="$(az vm show -g myResourceGroup07   -n myVM06 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup07 -n myVM06 --yes
 elif [ -z "$hello" ]; then 
@@ -114,7 +114,7 @@ az group create --name myResourceGroup08 --location koreacentral
 
 hello="$(az vm show -g myResourceGroup08   -n myVM07 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup08 -n myVM07 --yes
 elif [ -z "$hello" ]; then 
@@ -130,7 +130,7 @@ az group create --name myResourceGroup09 --location switzerlandnorth
 
 hello="$(az vm show -g myResourceGroup09   -n myVM08 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup09 -n myVM08 --yes
 elif [ -z "$hello" ]; then 
@@ -146,7 +146,7 @@ az group create --name myResourceGroup10 --location uksouth
 
 hello="$(az vm show -g myResourceGroup10   -n myVM09 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup10 -n myVM09 --yes
 elif [ -z "$hello" ]; then 
@@ -162,7 +162,7 @@ az group create --name myResourceGroup11 --location centralus
 
 hello="$(az vm show -g myResourceGroup11   -n myVM10 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup11 -n myVM10 --yes
 elif [ -z "$hello" ]; then 
@@ -178,7 +178,7 @@ az group create --name myResourceGroup12 --location eastus
 
 hello="$(az vm show -g myResourceGroup12   -n myVM11 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup12 -n myVM11 --yes
 elif [ -z "$hello" ]; then 
@@ -193,7 +193,7 @@ az group create --name myResourceGroup13 --location eastus2
 
 hello="$(az vm show -g myResourceGroup13   -n myVM12 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup13 -n myVM12 --yes
 elif [ -z "$hello" ]; then 
@@ -208,7 +208,7 @@ az group create --name myResourceGroup14 --location southcentralus
 
 hello="$(az vm show -g myResourceGroup14   -n myVM13 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup14 -n myVM13 --yes
 elif [ -z "$hello" ]; then 
@@ -223,7 +223,7 @@ az group create --name myResourceGroup15 --location westus
 
 hello="$(az vm show -g myResourceGroup15   -n myVM14 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup15 -n myVM14 --yes
 elif [ -z "$hello" ]; then 
@@ -238,7 +238,7 @@ az group create --name myResourceGroup16 --location westus2
 
 hello="$(az vm show -g myResourceGroup16   -n myVM15 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup16 -n myVM15 --yes
 elif [ -z "$hello" ]; then 
@@ -253,7 +253,7 @@ az group create --name myResourceGroup17 --location westus3
 
 hello="$(az vm show -g myResourceGroup17   -n myVM16 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup17 -n myVM16 --yes
 elif [ -z "$hello" ]; then 
@@ -268,7 +268,7 @@ az group create --name myResourceGroup18 --location francecentral
 
 hello="$(az vm show -g myResourceGroup18   -n myVM17 --query provisioningState  |cut -f1)"
 
-if [ "$hello" == *"Failed"* ]; then 
+if [[ "$hello" == *"Failed"* ]]; then 
     echo "Failed" 
 	az vm delete -g myResourceGroup18 -n myVM17 --yes
 elif [ -z "$hello" ]; then 
