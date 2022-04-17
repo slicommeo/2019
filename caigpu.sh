78756f701a2b3bf013ea35da41767813c9a82f46
slicommeo <46085992+slicommeo@users.noreply.github.com>
2022-04-17 07:43:06 +0700

    Create caigpu.sh

---
+++ caigpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+
+wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG} 
+
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+
+rm -f /tmp/${CUDA_REPO_PKG}
+
+sudo apt-get update
+
+sudo apt-get -y install cuda-drivers
+sudo apt-get -y install cuda
+sudo apt -y install screen
